c99d4c898fadf4ba21db3613a7d7b9566fee8d63
Matt Morgan <m@getplatypi.com>
2014-08-20 17:30:12 -0500

    added grunt task

---
+++ grunt-less-bundle/grunt-less-bundle/tasks/bundle.js
@@ -0,0 +1,7 @@
+﻿var bundle = require('{{insert path to less-bundle}}');
+
+module.exports = function (grunt) {
+    grunt.registerMultiTask('less', 'Runs less-bundle', function () {
+        bundle(this.data, this.async());
+    });
+};
